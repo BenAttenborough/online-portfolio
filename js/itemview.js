5013381307af4d579abcd074895a085caf2f6ac7
Ben Attenborough <battenborough@gmail.com>
2017-03-11 12:28:35 +0000

    Work on menus

--- js/itemview.js
+++ js/itemview.js
@@ -2,14 +2,6 @@
  * Created by ben on 25/02/2017.
  */
 
-console.log("Item view working");
-//$(".card").click(function(){
-//   console.log("Card clicked");
-//   console.log(this);
-//   $("#overlay").show();
-//   $("body").css('overflow','hidden');
-//});
-
 $(".card-close-btn").click(function(){
    $("#overlay").hide();
    $("body").css('overflow','visible');
@@ -19,23 +11,25 @@ var cards = $(".card");
 
 var title;
 var text;
+var imgSrc;
 var imgSm;
 var imgMd;
 var imgL;
 var imgXl;
+var git;
+var livesite;
 for (var i = 0; i < cards.length; i++) {
    //console.log(cards.eq(i).children(".meta").eq(0).data("title"));
    cards.eq(i).click(function() {
-      console.log("Card clicked");
-      console.log($(this).children(".meta").data("title"));
-      console.log($(this).children(".meta").data("text"));
-      console.log($(this).children(".meta").data("img"));
       title = $(this).children(".meta").data("title");
       text = $(this).children(".meta").data("text");
       imgSm = $(this).children(".meta").data("img-sm");
       imgMd = $(this).children(".meta").data("img-md");
       imgL = $(this).children(".meta").data("img-l");
       imgXl = $(this).children(".meta").data("img-xl");
+      git = $(this).children(".meta").data("git");
+      livesite = $(this).children(".meta").data("live");
+
       imgSrc = "<img src=\"img/html-video-600px.jpg\" ";
       imgSrc += "srcset=\"";
       imgSrc += imgSm + " 400w, ";
@@ -45,12 +39,11 @@ for (var i = 0; i < cards.length; i++) {
       imgSrc += "alt=\"" + title + "\"";
       imgSrc += "class=\"project-image\">";
 
-
       $("#overlay-card-title").html("<h2>" + title + "</h2>");
-      $("#overlay-card-text").html("<p>" + text + "</p>");
+      $("#overlay-card-text").html(text);
       $("#overlay-card-img").html(imgSrc);
-
-      console.log(imgXl);
+      console.log($("#overlay-card-github").attr("href", git));
+      console.log($("#overlay-card-live").attr("href", livesite));
 
       $("#overlay").show();
       $("body").css('overflow', 'hidden');
