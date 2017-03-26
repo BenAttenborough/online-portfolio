3db07a6f778113d49701880061beb52e1967cf1e
Ben Attenborough <battenborough@gmail.com>
2017-03-26 15:10:32 +0100

    Menu transition

--- js/menu.js
+++ js/menu.js
@@ -6,4 +6,6 @@ $("#mobile-nav-links").hide();
 
 $("#mobile-nav-button").click(function () {
     $("#mobile-nav-links").slideToggle();
+    $(".hamburger-icon").toggleClass( "hide" );
+    $(".hamburger-icon-close").toggleClass( "hide" );
 });
